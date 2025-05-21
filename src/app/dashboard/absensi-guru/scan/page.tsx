72b0a93416eb6d1b9e676775bca97ae4313103cb
ALFANAHEL <lehan.virtual@gmail.com>
2025-05-21 12:58:46 +0700

    Update page.tsx

--- src/app/dashboard/absensi-guru/scan/page.tsx
+++ src/app/dashboard/absensi-guru/scan/page.tsx
@@ -491,11 +491,12 @@ export default function TeacherAttendanceScan() {
             </div>
             
             {/* Recognized teacher */}
-            {recognizedTeacher && <div className="p-4 bg-blue-50 rounded-lg mb-4 border border-blue-200" data-unique-id="079b614b-8eea-4002-8e0c-117536b80737" data-file-name="app/dashboard/absensi-guru/scan/page.tsx">
+            {recognizedTeacher && 
+              <center><div className="p-4 bg-blue-50 rounded-lg mb-4 border border-blue-200" data-unique-id="079b614b-8eea-4002-8e0c-117536b80737" data-file-name="app/dashboard/absensi-guru/scan/page.tsx">
                 <h3 className="text-lg font-semibold text-blue-800" data-unique-id="3685f6bc-8cbe-49d8-8954-1e7df183a1a1" data-file-name="app/dashboard/absensi-guru/scan/page.tsx" data-dynamic-text="true">{recognizedTeacher.name}</h3>
-                <p className="text-sm text-blue-600" data-unique-id="2011a9ff-236b-44ea-831d-c27ef674a9d9" data-file-name="app/dashboard/absensi-guru/scan/page.tsx" data-dynamic-text="true"><span className="editable-text" data-unique-id="d63d5544-1a6b-41ba-9257-e9457b13f38b" data-file-name="app/dashboard/absensi-guru/scan/page.tsx">NIK: </span>{recognizedTeacher.nik}</p>
-                <p className="text-sm text-blue-600" data-unique-id="f5a19de5-8351-4ece-8e86-1946ae997569" data-file-name="app/dashboard/absensi-guru/scan/page.tsx" data-dynamic-text="true"><span className="editable-text" data-unique-id="85e40a5d-3d0d-4e2e-b575-70051340847b" data-file-name="app/dashboard/absensi-guru/scan/page.tsx">Jabatan: </span>{recognizedTeacher.role}</p>
-              </div>}
+                <p className="text-sm text-blue-700" data-unique-id="2011a9ff-236b-44ea-831d-c27ef674a9d9" data-file-name="app/dashboard/absensi-guru/scan/page.tsx" data-dynamic-text="true"><span className="editable-text" data-unique-id="d63d5544-1a6b-41ba-9257-e9457b13f38b" data-file-name="app/dashboard/absensi-guru/scan/page.tsx">NIK : </span>{recognizedTeacher.nik}</p>
+                <p className="text-sm text-blue-700" data-unique-id="f5a19de5-8351-4ece-8e86-1946ae997569" data-file-name="app/dashboard/absensi-guru/scan/page.tsx" data-dynamic-text="true"><span className="editable-text" data-unique-id="85e40a5d-3d0d-4e2e-b575-70051340847b" data-file-name="app/dashboard/absensi-guru/scan/page.tsx">Jabatan : </span>{recognizedTeacher.role}</p>
+              </div></center>}
           </div>
           
           <div className="p-6 grid grid-cols-1 md:grid-cols-2 gap-4" data-unique-id="584cd3d7-b43f-4246-8616-cbf497b37fac" data-file-name="app/dashboard/absensi-guru/scan/page.tsx" data-dynamic-text="true">
